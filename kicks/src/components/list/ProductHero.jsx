5c4a44b61ed87925ce1f3d2e4764f9e0a5efd9c8
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-02-16 20:32:44 +0100

    commit 33

--- kicks/src/components/list/ProductHero.jsx
+++ kicks/src/components/list/ProductHero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { images } from "../../data/ListedData";
 
 const ProductHero = () => {
   return (
     <div className="section-two">
       <div className="relative">
-        <img src={} alt="product hero image" className="w-full h-full object-cover" />
+        <img src={images.Hero} alt="product hero image" className="w-full h-full object-cover" />
         <div className=" lg:w-1/4 sm:w-1/2 w-1/2">
           <h4 className="text-2xl text-gray-300 font-medium normal-case mb-4">Limited time only</h4>
           <h2 className="text-6xl text-white font-Rubik font-bold mb-4">get 30% off</h2>
--- kicks/src/data/ListedData.js
+++ kicks/src/data/ListedData.js
@@ -1,33 +1,55 @@
+import Hero from "../assets/List/hero.jpg";
 import Shoe5 from "../assets/Home/shoe5.png";
+import Shoe6 from "../assets/List/shoe6.jpg";
+import Shoe7 from "../assets/List/shoe7.jpg";
+import Shoe8 from "../assets/List/shoe8.jpg";
+import Shoe9 from "../assets/List/shoe9.jpg";
+import Shoe10 from "../assets/List/shoe10.jpg";
+import Shoe11 from "../assets/List/shoe11.jpg";
+import Shoe12 from "../assets/List/shoe12.jpg";
+import Shoe13 from "../assets/List/shoe13.jpg";
+import Shoe14 from "../assets/List/shoe14.jpg";
 
-export const images = {};
+export const images = {
+  Hero,
+  Shoe5,
+  Shoe6,
+  Shoe7,
+  Shoe8,
+  Shoe9,
+  Shoe10,
+  Shoe11,
+  Shoe12,
+  Shoe13,
+  Shoe14,
+};
 
 export const ProductDrop = [
   {
     id: 1,
     type: "new",
-    image: Shoe1,
+    // image: Shoe1,
     name: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: "125",
   },
   {
     id: 2,
     type: "featured",
-    image: Shoe5,
+    // image: Shoe5,
     name: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: "150",
   },
   {
     id: 3,
     type: "upcoming",
-    image: Shoe3,
+    // image: Shoe3,
     name: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: "110",
   },
   {
     id: 4,
     type: "special",
-    image: Shoe4,
+    // image: Shoe4,
     name: "ADIDAS 4DFWD X PARLEY RUNNING SHOES",
     price: "80",
   },
