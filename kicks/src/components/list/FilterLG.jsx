5c8610100d44ba5a167baeb2ad2f1274437df70c
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-02-18 21:26:21 +0100

    commit 36

--- kicks/src/components/list/FilterLG.jsx
+++ kicks/src/components/list/FilterLG.jsx
@@ -4,18 +4,20 @@ import React from "react";
 const FilterLG = () => {
   return (
     <div className="">
+      <h3 className="text-3xl font-bold mb-12">filters</h3>
       <div className="">
-        <h3 className="">filters</h3>
-        <div className="">
-          <div className="flex justify-between gap-x-7">
-            <span className="">refine by</span>
-            <ArrowDropDown />
-          </div>
-          <div className="">
-            <div className=""></div>
-            <div className=""></div>
-          </div>
+        <div className="flex justify-between gap-x-96 mb-5">
+          <span className="text-xl uppercase font-Rubik font-bold">refine by</span>
+          <ArrowDropDown sx={{ fontSize: "2rem" }} />
         </div>
+        <div className="flex gap-x-6 items-center">
+          <div className="py-3 px-7 text-center flex text-xl font-Rubik bg-blue-600 text-white rounded-2xl">men</div>
+          <div className="py-3 px-7 text-center flex text-xl font-Rubik bg-blue-600 text-white rounded-2xl">casual</div>
+        </div>
+      </div>
+      <div className="flex justify-between gap-x-96 my-10">
+        <span className="text-xl uppercase font-Rubik font-bold">size</span>
+        <ArrowDropDown sx={{ fontSize: "2rem" }} />
       </div>
     </div>
   );
--- kicks/src/components/list/ProductList.jsx
+++ kicks/src/components/list/ProductList.jsx
@@ -5,7 +5,7 @@ import FilterLG from "./FilterLG";
 const ProductList = () => {
   return (
     <div className="section !pt-2">
-      <div className="sm:hidden flex justify-between items-center w-full">
+      <div className="lg:hidden flex justify-between items-center w-full">
         <div className="text-2xl uppercase cursor-pointer font-Rubik font-medium bg-white rounded-2xl py-4 px-7 gap-x-14 flex items-center justify-between">
           filter <Shuffle sx={{ fontSize: "2rem" }} />
         </div>
@@ -19,13 +19,13 @@ const ProductList = () => {
           <h2 className="text-5xl font-Rubik font-semibold">life style shoes</h2>
           <span className="text-xl font-medium">122 items</span>
         </div>
-        <div className=" sm:flex hidden cursor-pointer text-2xl uppercase font-Rubik font-semibold bg-white rounded-2xl py-4 px-7 gap-x-14 items-center justify-between">
+        <div className=" lg:flex hidden cursor-pointer text-2xl uppercase font-Rubik font-semibold bg-white rounded-2xl py-4 px-7 gap-x-14 items-center justify-between">
           trending <ArrowDropDown sx={{ fontSize: "2rem" }} />
         </div>
       </div>
 
       <div className="flex items-center gap-x-10 mt-10">
-        <div className="flex-[2] sm:flex hidden">
+        <div className="flex-[2] lg:flex hidden">
           <FilterLG />
         </div>
         <div className="flex-[5]">hello</div>
