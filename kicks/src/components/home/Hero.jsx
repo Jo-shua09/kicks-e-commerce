ec8d5f3a52c4b390375c6d9c7d08a15facd2e8c8
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-03-08 11:21:17 +0100

    commit 61

--- kicks/src/components/home/Hero.jsx
+++ kicks/src/components/home/Hero.jsx
@@ -21,14 +21,14 @@ const Hero = () => {
               src={images.Hero}
               loading="lazy"
               alt="Hero Image"
-              className="w-full overflow-hidden sm:h-[65rem] lg:h-full h-[50rem] object-cover rounded-[5rem]"
+              className="w-full overflow-hidden sm:h-[65rem] lg:h-full h-[57rem] object-cover rounded-[5rem]"
             />
             <h3 className="absolute normal-case font-medium bg-gray-950 rounded-t-2xl text-white p-5 sm:px-10 px-5 items-center flex text-base top-1/4  -left-20 sm:-left-24  rotate-90">
               Nike product of the year
             </h3>
             <div className="flex items-end justify-between absolute bottom-10 sm:bottom-20 sm:px-16 px-8 lg:px-20 w-full right-0 left-0">
               <div className="flex flex-col items-start gap-y-3 sm:w-1/2 w-3/6 lg:w-1/3">
-                <h2 className="sm:text-7xl text-6xl font-bold text-white uppercase font-Rubik">NIKE AIR MAX</h2>
+                <h2 className="md:text-7xl sm:text-6xl text-5xl text-nowrap font-bold text-white uppercase font-Rubik">NIKE AIR MAX</h2>
                 <p className="sm:text-3xl text-2xl font-medium pb-5 text-gray-50 normal-case">
                   Nike introducing the new air max for everyone's comfort
                 </p>
