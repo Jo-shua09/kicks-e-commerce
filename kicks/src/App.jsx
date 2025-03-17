efc0480939bee299c0f162ac157099f2023f6ad2
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-03-17 10:37:11 +0100

    added vercel web analytics

--- kicks/src/App.jsx
+++ kicks/src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import List from "./pages/List";
