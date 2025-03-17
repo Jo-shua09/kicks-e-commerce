febb211ee59da38795137df51d4c72e5df428590
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-03-17 10:26:52 +0100

    correction made to the loader

--- kicks/src/loader/Loader.jsx
+++ kicks/src/loader/Loader.jsx
@@ -1,16 +1,5 @@
 import React from "react";
 
-import { SvgIcon } from "@mui/material";
-
-function KIcon(props) {
-  return (
-    <SvgIcon {...props} viewBox="0 0 24 24">
-      <text x="4" y="20" fontSize="20" fontWeight="bold" fill="currentColor">
-        K
-      </text>
-    </SvgIcon>
-  );
-}
 const Loader = () => {
   return (
     <div className="flex z-[9999999999] justify-center items-center min-h-screen w-full fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#e7e7e3]">
